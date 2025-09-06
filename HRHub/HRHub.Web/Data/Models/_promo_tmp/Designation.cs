cd8258e5d0ca7590238c17f79b3d569af6fd2ee1
mohammadhossainjoy <joyh2755@gmail.com>
2025-09-06 21:15:45 +0600

    feat(promotion): Admin create promotion with rules and history

---
+++ HRHub/HRHub.Web/Data/Models/_promo_tmp/Designation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRHub.Web.Data.Models._promo_tmp;
+
+public partial class Designation
+{
+    public int DesignationId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public int? Grade { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+    public virtual ICollection<Promotion> PromotionNewDesignations { get; set; } = new List<Promotion>();
+
+    public virtual ICollection<Promotion> PromotionOldDesignations { get; set; } = new List<Promotion>();
+}
---
+++ HRHub/HRHub.Web/Data/Models/_promo_tmp/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRHub.Web.Data.Models._promo_tmp;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string EmpNo { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public DateOnly JoinDate { get; set; }
+
+    public int CompanyId { get; set; }
+
+    public int DepartmentId { get; set; }
+
+    public int DesignationId { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string? AspNetUserId { get; set; }
+
+    public virtual Designation Designation { get; set; } = null!;
+
+    public virtual ICollection<Promotion> Promotions { get; set; } = new List<Promotion>();
+}
